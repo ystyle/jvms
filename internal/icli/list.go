4ead1a01c9036334685910f6fb17dae3777b9312
renniemaharaj <rvesprey@gmail.com>
2026-07-02 11:54:13 -0400

    fmt string lint

--- internal/icli/list.go
+++ internal/icli/list.go
@@ -23,7 +23,7 @@ func list(config *models.Config) *cli.Command {
 				} else {
 					str = fmt.Sprintf("%s    %d) %s", str, i+1, version)
 				}
-				fmt.Printf(str + "\n")
+				fmt.Printf("%s", str+"\n")
 			}
 			if len(v) == 0 {
 				fmt.Println("No installations recognized.")
