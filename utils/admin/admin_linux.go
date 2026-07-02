816eb673107abdc3eeab9054c94b3e846d7a2411
renniemaharaj <rvesprey@gmail.com>
2026-07-02 12:40:06 -0400

    chore: improved comment

--- utils/admin/admin_linux.go
+++ utils/admin/admin_linux.go
@@ -3,7 +3,7 @@
 package admin
 
 // IsAdmin checks if the current process has administrative privileges
+// checks are only implemented on Windows to allow development on Linux.
 func IsAdmin() bool {
-	// This returns stub for non-windows platforms. It always returns false otherwise.
 	return false // To support development on linux without importing "golang.org/x/sys/windows" on non-windows platforms
 }
