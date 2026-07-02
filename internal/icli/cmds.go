de61c5465688b314474fd2ead3131a444da5ae05
renniemaharaj <rvesprey@gmail.com>
2026-07-02 15:55:46 -0400

    chore: simplified switch prerequisites

--- internal/icli/cmds.go
+++ internal/icli/cmds.go
@@ -1,7 +1,6 @@
 package icli
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -19,30 +18,38 @@ var (
 	DefaultJavaHome = filepath.Join(os.Getenv("ProgramFiles"), "jdk")
 )
 
-// ensureConfigInitializedAndIsAdmin checks if the config is initialized and if the user has admin privileges.
-// If the config is not initialized and the user is an admin, it attempts to initialize itself automatically.
-func ensureConfigInitializedAndIsAdmin(config *models.Config) func(*cli.Context) error {
-	return func(c *cli.Context) error {
-		if config.JavaHomeNotSet() {
-			if admin.IsAdmin() {
-				fmt.Println("Jvms not initialized. Attempting to initialize jvms automatically...")
-				if err := init_(config).Run(c); err != nil {
-					return fmt.Errorf("could not initialize jvms automatically. Please run \"jvms init\" to initialize jvms\n%v", err)
-				}
-				return nil
-			}
-			return errors.New("jvms is not initialized and you're not running as administrator.\n" +
-				"Please run \"jvms init\" to initialize jvms or run the command as administrator to allow automatic initialization. \n" +
-				"You may also specify the JAVA_HOME location using the --java_home flag when running \"jvms init\".")
-		}
-		return nil
-	}
-}
-
 // Commands Register all commands
 func Commands(c *models.Config) []cli.Command {
 	return []cli.Command{
 		*switch_(c), *install(c), *remove(c), *init_(c),
 		*proxy(c), *list(c), *use(c), *rls(c),
+	}
+}
+
+// switchPrerequisites checks if the config is initialized and if the user has admin privileges.
+// If the config is not initialized and the user is an admin, it attempts to initialize itself automatically.
+func switchPrerequisites(config *models.Config) func(*cli.Context) error {
+	return func(c *cli.Context) error {
+		if !config.JavaHomeNotSet() {
+			return nil
+		}
+
+		if !admin.IsAdmin() {
+			return fmt.Errorf("jvms is not initialized and you're not running as administrator.\n" +
+				"Please run \"jvms init\" to initialize jvms or run the command as administrator to allow automatic initialization.\n" +
+				"You may also specify the JAVA_HOME location using the --java_home flag when running \"jvms init\".",
+			)
+		}
+
+		fmt.Println("Jvms not initialized. Attempting to initialize jvms automatically...")
+
+		if err := init_(config).Run(c); err != nil {
+			return fmt.Errorf(
+				`could not initialize jvms automatically. Please run "jvms init" manually: %w`,
+				err,
+			)
+		}
+
+		return nil
 	}
 }
--- internal/icli/switch.go
+++ internal/icli/switch.go
@@ -44,7 +44,7 @@ func switch_(config *models.Config) *cli.Command {
 func switchFunc(config *models.Config) func(*cli.Context) error {
 	return func(c *cli.Context) error {
 		// Ensure the config is initialized with java_home & isAdmin
-		if err := ensureConfigInitializedAndIsAdmin(config)(c); err != nil {
+		if err := switchPrerequisites(config)(c); err != nil {
 			return err
 		}
 		v := strings.TrimSpace(c.Args().Get(0))
